e4455119e486097c0ad669d3e80eda9b7cca6c5f
Preston <childrep@vt.edu>
2021-08-04 22:45:09 -0400

    Add intake commands for auto.

--- src/main/java/frc/robot/commands/intake/RunIntakeAuto.java
+++ src/main/java/frc/robot/commands/intake/RunIntakeAuto.java
@@ -2,16 +2,13 @@ package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.hid.HID;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 
-public class RunIntakeTeleop extends CommandBase {
+public class RunIntakeAuto extends CommandBase {
     private final IntakeSubsystem intake;
-    private final HID hid;
-    
-    public RunIntakeTeleop(IntakeSubsystem intake, HID hid) {
+
+    public RunIntakeAuto(IntakeSubsystem intake) {
         this.intake = intake;
-        this.hid = hid;
 
         addRequirements(intake);
     }
@@ -23,16 +20,12 @@ public class RunIntakeTeleop extends CommandBase {
 
     @Override
     public void execute() {
-        if (hid.runIntakeButton())
             intake.runIntake(Constants.kIntakePercent);
-        else if (hid.runOuttakeButton())
-            intake.runIntake(Constants.kSpitOutPercent);
-        else
-            intake.stop();
     }
 
     @Override
     public void end(boolean interrupted) {
         intake.stop();
     }
+    
 }
